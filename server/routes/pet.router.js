57dc1ff11bfe7998594c3a51f9bbe2386e7713f1
Michael Gregoire <mdgregoire@gmail.com>
2018-02-20 11:50:13 -0600

    pathing is working

--- server/routes/pet.router.js
+++ server/routes/pet.router.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
     ORDER BY owner.last_name, pet.is_checked_in;`
     pool.query(queryText)
         .then((result) => {
-            console.log('"/" Results of GET query for pet: ', result.rows); 
+            console.log('"/" Results of GET query for pet: ', result.rows);
             res.send(result.rows);
         })
         .catch((err) => {
@@ -20,12 +20,12 @@ router.get('/', (req, res) => {
             res.sendStatus(500);
         });
 
-}); 
+});
 // End GET route.
 
 // POST route start.
 router.post('/', (req,res) => {
-   
+
     const queryText = 'INSERT INTO pet (pet_name, breed, color, owner_id) VALUES ($1, $2, $3, $4)';
     pool.query(queryText, [req.body.pet_name, req.body.breed, req.body.color, req.body.owner_id])//Start of initial post query
         .then((result) => {
@@ -45,12 +45,12 @@ router.put ('/:id', (req, res) => {
     let queryText = `UPDATE pet SET  is_checked_in = $1 WHERE id = $2`;
     pool.query(queryText, [req.body.is_checked_in, req.params.id])
         .then((results) =>{
-            console.log('LOGING REQ.BODY.IS_CHECKED_IN: ', req.body.is_checked_in);    
+            console.log('LOGING REQ.BODY.IS_CHECKED_IN: ', req.body.is_checked_in);
             if (req.body.is_checked_in == 'true') {
                 let queryText = `INSERT INTO visits (pet_id) VALUES ($1)`
                              pool.query(queryText, [req.params.id])
                              .then((results) =>{
-                                 console.log('IN INSERT NEW TIME TO VISITS');        
+                                 console.log('IN INSERT NEW TIME TO VISITS');
                                  res.sendStatus(201);
                              })
                              .catch((err) =>{
@@ -62,11 +62,11 @@ router.put ('/:id', (req, res) => {
                 let queryText = `SELECT visits.id FROM visits WHERE pet_id = $1 AND check_out_date is NULL;`
                              pool.query(queryText, [req.params.id])
                              .then((results) =>{
-                                 
+
                                 let queryText = `UPDATE visits SET check_out_date = now() WHERE pet_id = $1 AND visits.id = $2;`
                                 pool.query(queryText, [req.params.id, results.rows[0].id])
                                     .then((results) =>{
-                                        console.log('IN INSERT CHECKOUT TIME TO VISITS');        
+                                        console.log('IN INSERT CHECKOUT TIME TO VISITS');
                                         res.sendStatus(201);
                                     })
                                     .catch((err) =>{
@@ -74,7 +74,7 @@ router.put ('/:id', (req, res) => {
                                         res.sendStatus(500);
                                     });
 
-                             
+
                              })
                              .catch((err) =>{
                                  console.log('error making update pet status query:', err);
@@ -95,7 +95,7 @@ router.put ('/update/:id', (req, res) => {
     let queryText = `UPDATE pet SET pet_name =$1, breed =$2, color=$3 WHERE id = $4`;
     pool.query(queryText, [req.body.pet_name, req.body.breed, req.body.color, req.params.id])
     .then((results) =>{
-        console.log('updated pet info: ', results);        
+        console.log('updated pet info: ', results);
         res.send(results);
     })
     .catch((err) =>{
@@ -110,7 +110,7 @@ router.delete ('/:id', (req, res) => {
     let queryText = `DELETE FROM pet WHERE id = $1`;
     pool.query(queryText, [req.params.id])
     .then((results) =>{
-        console.log('query delete results: ', results);        
+        console.log('query delete results: ', results);
         res.send(results);
     })
     .catch((err) =>{
@@ -120,4 +120,4 @@ router.delete ('/:id', (req, res) => {
 });
 // End DELETE route
 
-module.exports = petRouter;
+module.exports = router;
